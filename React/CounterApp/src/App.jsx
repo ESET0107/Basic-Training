f49d756fe5682ed09bcd21eebbe835c50ea656ee
NVKoushik <nvkoushik18@gmail.com>
2025-09-04 17:42:43 +0530

    added new files

--- React/CounterApp/src/App.jsx
+++ React/CounterApp/src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const user = {name : 'nvk',age : 23,place:'mangalore'}
+  const{name,...rest} = user
+  console.log(name)
+  console.log(user)
+  
+
   let count = 0;
   const increment = ()=>{
     count++;
@@ -13,11 +19,12 @@ function App() {
     count--;
     document.getElementById("123").innerText = count
   }
+  
   return(
     <div>
-      <h1 id = "123">0</h1>
-      <button onClick={increment}>INCREMENT</button>
-      <button onClick={decrement}>DECREMENT</button>
+      <h1 id = "123" style={{ color: 'black' }}>0</h1>
+      <button style={{ backgroundColor: 'aqua', color: 'white' }}onClick={increment}>INCREMENT</button>
+      <button style={{ backgroundColor: 'crimson', color: 'white' }}onClick={decrement}>DECREMENT</button>
     </div>
   )
 }
