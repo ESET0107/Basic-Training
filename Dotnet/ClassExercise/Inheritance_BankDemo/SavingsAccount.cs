603e640b71528fbc44518816932d2e0902159851
NVKoushik <nvkoushik18@gmail.com>
2025-09-24 10:18:04 +0530

    added files

---
+++ Dotnet/ClassExercise/Inheritance_BankDemo/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_BankDemo
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public void Init(string accountNumber, string holderName, double balance = 0.0, double interestRate = 0.05)
+        {
+            base.Init(accountNumber, holderName, balance);
+            InterestRate = interestRate;
+        }
+
+        public void ApplyInterest()
+        {
+            double interest = Balance * InterestRate;
+            Balance += interest;
+            Console.WriteLine($"Interest of ₹{interest} applied. New Balance: ₹{Balance}");
+        }
+    }
+}
