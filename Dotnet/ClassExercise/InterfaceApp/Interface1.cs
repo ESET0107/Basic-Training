23bec39b5115e5a46b442bc2faf42caba4fe6bab
NVKoushik <nvkoushik18@gmail.com>
2025-09-24 17:40:04 +0530

    added files

---
+++ Dotnet/ClassExercise/InterfaceApp/Interface1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceApp
+{
+    internal interface IInterface1
+    {
+        void Method1();
+    }
+    class Class3 : IInterface1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1 implementation in Class3");
+        }
+    }
+
+    class Class4 : IInterface1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1 implementation in Class4");
+        }
+    }
+}
---
+++ Dotnet/ClassExercise/InterfaceApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace InterfaceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IInterface1 obj1 = new Class3();
+            obj1.Method1();
+            IInterface1 obj2 = new Class4();
+            obj2.Method1();
+        }
+    }
+}
